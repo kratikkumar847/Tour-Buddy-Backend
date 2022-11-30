1381753ba4d75d9fca52d022f008b893e60b3038
NileshDeshmukh09 <nileshdeshmukh0908@gmail.com>
2022-11-30 22:06:33 +0530

    user Model

---
+++ models/user.model.js
@@ -0,0 +1,44 @@
+
+/**
+* Schema for the POST Model will be provided Here
+*/
+const mongoose = require("mongoose");
+const userSchema = new mongoose.Schema({
+
+    name : {
+        type: String, 
+        required : true 
+    },
+
+    userID: {
+        type: String,
+        unique : true,
+        required: true
+    },
+
+    email : {
+        type : String, 
+        unique : true,
+        required : true
+    },
+
+    password :{
+        type:String,
+        required:true
+    },
+
+    member : {
+        type : Array,
+        default :[]
+    }
+
+});
+
+/* These will automatically generates the created and updated fields */
+userSchema.set('timestamps' , true);
+
+
+
+module.exports = mongoose.model("User", userSchema);
+
+
