0efcda497b64fbdc954cc7fc80ee3c5f422442bd
NileshDeshmukh09 <nileshdeshmukh0908@gmail.com>
2022-12-02 13:20:53 +0530

    Connected URL and added to .env

--- index.js
+++ index.js
@@ -14,18 +14,7 @@ app.use(
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
-// const DB = "mongodb+srv://kratik:1234@cluster0.qqggtnr.mongodb.net/tourbuddydb?retryWrites=true&w=majority";
-const DB = "mongodb://kratik:1234@ac-iiwxcnu-shard-00-00.qqggtnr.mongodb.net:27017,ac-iiwxcnu-shard-00-01.qqggtnr.mongodb.net:27017,ac-iiwxcnu-shard-00-02.qqggtnr.mongodb.net:27017/tourbuddydb?ssl=true&replicaSet=atlas-c5unyd-shard-0&authSource=admin&retryWrites=true&w=majority";
-// mongoose.connect( DB, ()=>{
-//     try{
-//         console.log("MongoDB connected");   
-//     }
-//     catch(error){
-//         console.log(error);
-//     }
-// });
-
-mongoose.connect(DB, {
+mongoose.connect(process.env.DB, {
   useNewUrlParser: "true",
 })
 mongoose.connection.on("error", err => {
